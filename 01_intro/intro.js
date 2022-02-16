e1b8b66c65bb01b5808cdbea3a1bfc63b4a9c2c5
bioss7 <fireleny@gmail.com>
2022-02-16 18:45:17 +0700

    02_sync

--- 01_intro/intro.js
+++ 01_intro/intro.js
@@ -19,7 +19,7 @@ const nativNull = () => null;
 
 console.log(sum(41, 1));
 expect(sum(41, 1)).toBe(42); // Success
-expect(sum(41, 1)).toBe(43); // Value is 42, but expectation is 43
+// expect(sum(41, 1)).toBe(43); // Value is 42, but expectation is 43
 
 // Экспорт функций 
 module.exports = {sum, nativNull} 
